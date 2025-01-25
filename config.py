dba8a253f46ff6642ad3d4992adbe56d9af30b02
sanggusti <gustiowinata0@gmail.com>
2025-01-25 22:23:16 +0700

    Main starter files

---
+++ config.py
@@ -0,0 +1,9 @@
+COHERE_API_KEY = "your-cohere-key"
+DS_CONFIG = {
+    "base_path": "https://demo.docusign.net/rest-api",
+    "access_token": "your-docusign-token",
+    "account_id": "your-account-id",
+    "client_id": "your-client-id",
+    "return_url": "http://localhost:8000/callback"
+}
+LANCEDB_URI = "data/healthcare.lancedb"
